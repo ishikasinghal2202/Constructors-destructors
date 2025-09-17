44001485f258adfbc2568f885e9781595b87c255
Ishika <singhalishika436@gmail.com>
2025-09-17 15:09:58 +0530

    Create Function overloading.cpp

---
+++ Function overloading.cpp
@@ -0,0 +1,28 @@
+//Ishika Singhal
+//24070123048
+#include <iostream>
+using namespace std;
+class Addition
+   {
+       
+       public:
+       int sum(int a, int b)
+       {
+           return a+b;
+       }
+       int sum(int a,int b, int c)
+       {
+           return a+b+c;
+       }
+   };
+   int main ( void)
+   {
+       Addition obj;
+       cout<<obj.sum(20,15)<<endl;
+       cout<<obj.sum(81,100,10);
+       return 0;
+   }
+
+//OUTPUT
+//35
+//191
